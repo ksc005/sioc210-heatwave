915e8e832d93f1457e9aa0bc6567b25c04e256a6
ksc005 <ksc001@ucsd.edu>
2024-11-19 11:25:29 -0800

    initial commit

---
+++ normal.m
@@ -0,0 +1,22 @@
+%% Document Header
+
+% Topic: SIOC 210 [FA24] Data Project Analysis of Normal Year
+
+% Date: November 2024
+
+% Author(s): Kathryn Chen, Soo yoon Kim
+
+% Author contributions: SYK accessed and downloaded the data; KC wrote the code.
+
+% Objective: To analyze ARGO float data in the Tasman Sea of a reference non-marine
+% heatwave year (Dec 2012-February 2013). This script will average
+% temperature across all floats at each depth, and output a data file of
+% this mean temperature-depth for use in comparison plots.
+
+%% Preview one of the float data files
+
+%% Read in all float data
+
+%% Average temperature across all floats at each depth
+
+%% Output data file of mean temperature-depth
