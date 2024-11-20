9e336d94b87af73703320322b87e0a2a34111435
ksc005 <ksc001@ucsd.edu>
2024-11-19 16:08:53 -0800

    start reading in normal floats

--- normal.m
+++ normal.m
@@ -14,45 +14,97 @@
 % this mean temperature-depth for use in comparison plots.
 
 %% Preview one of the float data files
-ncdisp("argo_tasman_2012_2013/D5901092_229.nc")
+input = "argo_tasman_2012_2013/D5901092_229.nc";
+ncdisp(input)
 
 % read some variables from the file
-lat = ncread("argo_tasman_2012_2013/D5901092_229.nc", "LATITUDE");
-long = ncread("argo_tasman_2012_2013/D5901092_229.nc", "LONGITUDE");
+temp = ncread(input, "TEMP");
+tempQC = double(string(ncread(input, "TEMP_QC")));
 
-temp = ncread("argo_tasman_2012_2013/D5901092_229.nc", "TEMP");
-tempQC = double(string(ncread("argo_tasman_2012_2013/D5901092_229.nc", "TEMP_QC")));
+pres = ncread(input, "PRES"); % pressure (dbars), so approximates depth (m)
+presQC = double(string(ncread(input, "PRES_QC")));
 
-pres = ncread("argo_tasman_2012_2013/D5901092_229.nc", "PRES"); % pressure (dbars), so approximates depth (m)
-presQC = double(string(ncread("argo_tasman_2012_2013/D5901092_229.nc", "PRES_QC")));
+lat = ones(length(temp),1) * ncread(input, "LATITUDE");
+long = ones(length(temp),1) * ncread(input, "LONGITUDE");
 
 % concatenate variables
-data = horzcat(temp, tempQC, pres, presQC);
+data = horzcat(lat, long, temp, tempQC, pres, presQC);
 
 % Quality check. If items did not pass QC, make them NaN.
 % QC codes reference:
 % http://www.argodatamgt.org/content/download/12096/80327/file/argo-dm-user-manual.pdf
 % page 57, reference table 2
 for i = 1:length(data)
-    if any(ismember([1 2 5 8], data(i,2))) == 1 & any(ismember([1 2 5 8], data(i,4))) == 1
+    if any(ismember([1 2 5 8], data(i,4))) == 1 & any(ismember([1 2 5 8], data(i,6))) == 1
         continue
     else
-        warning("Item %d, with temp %d and pressure %d, did not pass QC. Writing to NaN", i, ...
-            data(i,1), data(i,3))
-        data(i,1) = NaN;
+        warning("Item [lat %d, long %d, temp %d, pressure %d] did not pass QC. Writing to NaN", ...
+            data(i,1), data(i,2), data(i,3), data(i,5))
         data(i,3) = NaN;
+        data(i,5) = NaN;
     end
 end
 
 % plot temperature-depth
 figure()
-plot(temp, pres, 'LineWidth', 2)
+plot(data(:,3), data(:,5), 'LineWidth', 2)
 fontsize(16, 'points')
 xlabel("Temperature [˚C]"), ylabel("Depth (m)"), title("Sample Argo Float T-D")
 grid on
 set(gca, 'YDir','reverse')
 
 %% Read in all float data
+files = dir('argo_tasman_2012_2013/');
+
+tic
+for i = 3:length(files)
+    % set input file name
+    input = [files(i).folder '/' files(i).name];
+
+    % read variables from the file
+    temp = ncread(input, "TEMP");
+    tempQC = double(string(ncread(input, "TEMP_QC")));
+    
+    pres = ncread(input, "PRES"); % pressure (dbars), so approximates depth (m)
+    presQC = double(string(ncread(input, "PRES_QC")));
+    
+    lat = ones(length(temp),1) .* ncread(input, "LATITUDE");
+    long = ones(length(temp),1) .* ncread(input, "LONGITUDE");
+    
+    % concatenate variables
+    data = horzcat(lat, long, temp, tempQC, pres, presQC);
+
+    % quality control. If items did not pass QC, make them NaN.
+    % QC codes reference:
+    % http://www.argodatamgt.org/content/download/12096/80327/file/argo-dm-user-manual.pdf
+    % page 57, reference table 2
+    for i = 1:length(data)
+        if any(ismember([1 2 5 8], data(i,4))) == 1 & any(ismember([1 2 5 8], data(i,6))) == 1
+            continue
+        else
+            warning("Item [lat %d, long %d, temp %d, pressure %d] in file %s did not pass QC. Writing to NaN", ...
+                data(i,1), data(i,2), data(i,3), data(i,5), input)
+            data(i,3) = NaN;
+            data(i,5) = NaN;
+        end
+    end
+
+    % save output table; concatenate all output tables from all floats
+    if exist('output', 'var') ~= 1
+        output = array2table(data, ...
+            'VariableNames', {'lat', 'long', 'temp', 'tempQC', 'pres', 'presQC'});
+    else
+        output2 = array2table(data, ...
+            'VariableNames', {'lat', 'long', 'temp', 'tempQC', 'pres', 'presQC'});
+       
+        output = [output; output2];
+    end
+
+    % print confirmation
+    disp(i)
+
+end
+toc
 
 %% Average temperature across all floats at each depth
 
