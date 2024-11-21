c28cb05a5a9cfb7d8ddadb12c64e7b1de3ba4703
ksc005 <ksc001@ucsd.edu>
2024-11-21 15:13:51 -0800

    start comparison file

---
+++ Tasman_Sea/comparison.m
@@ -0,0 +1,20 @@
+%% Document Header
+
+% Topic: Tasman Sea MHW – Comparison of MHW and Reference Year
+
+% Date: November 2024
+
+% Author(s): Kathryn Chen, Soo yoon Kim
+
+% Author contributions: SYK accessed and downloaded the original data; KC wrote the code.
+
+% Objective: To plot a comparison of heatwave and non-heatwave temperature-depth 
+% profiles. This script takes in mean temperature-depth files for a reference 
+% normal year (Dec 2012-Feb 2013) and the MHW year (Dec 2015-2016), and 
+% outputs a figure of T-D profiles showing both years side-by-side.
+
+%% Read in data files
+
+%% Create figure
+
+%% Save figure
