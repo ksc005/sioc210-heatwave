e81f1a6faeb70ec5962ab4df3fff9d1efa52a0c7
ksc005 <ksc001@ucsd.edu>
2024-12-02 13:35:14 -0800

    removed medians and added shaded standard errors to plots

--- Tasman_Sea/run.m
+++ Tasman_Sea/run.m
@@ -1,7 +1,8 @@
 clear all
 
 % define input directories and names of output files here
-inputs = ["data_v2/argo_tasman_2012_2013", "ref_2012_2013_v2"];
+inputs = ["data_v2/argo_tasman_2012_2013", "ref_2012_2013_v2";
+          "data_v2/argo_tasman_2015_2016", "mhw_2015_2016_v2"];
 
 % which analyses do you want to run?
 get_td_plot = 1; % temperature-depth processing
