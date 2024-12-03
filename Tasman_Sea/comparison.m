7ea205f4fb0ec5a82d8ca8ba755ce35632e31788
ksc005 <ksc001@ucsd.edu>
2024-12-02 16:31:57 -0800

    edited to plot monthly profiles

--- Tasman_Sea/comparison.m
+++ Tasman_Sea/comparison.m
@@ -31,46 +31,67 @@ print(['Tasman_' refYear '_' mhwYear '_temporal_res'], '-dpng')
 
 %% Figure: Temperature-Depth Profiles
 % Read in data files
-refTD = readtable([refYear '_temp_depth.csv']);
-mhwTD = readtable([mhwYear '_temp_depth.csv']);
+refTD = readtable([refYear '_monthly_temp_depth.csv']);
+mhwTD = readtable([mhwYear '_monthly_temp_depth.csv']);
 
 % Set parameters
-depthLim = 500; % to what depth you want to show on the plot
+%months = unique(mhwTD.floatMonth);
+months = [12 1 2];
+numMonths = length(months);
+xLim = [0, max([max(refTD.mean_temp), max(mhwTD.mean_temp)]) + 1];
+depthLim = 1000; % to what depth you want to show on the plot
 lineWidth = 3;
 refColorDark = [0.2 0.5 0.9 0.8];
 refColorLight = [0.2 0.5 0.9 0.2];
 mhwColorDark = [0.9 0.2 0.3 0.8];
 mhwColorLight = [0.9 0.2 0.3 0.2];
 
-% draw standard error shapes
-mhwSe1 = mhwTD.mean_temp + mhwTD.se;
-mhwSe2 = mhwTD.mean_temp - mhwTD.se;
-mhwY2 = [mhwTD.layerCenter; fliplr(mhwTD.layerCenter')'];
-mhwX2 = [mhwSe1; fliplr(mhwSe2')'];
-
-refSe1 = refTD.mean_temp + refTD.se;
-refSe2 = refTD.mean_temp - refTD.se;
-refY2 = [refTD.layerCenter; fliplr(refTD.layerCenter')'];
-refX2 = [refSe1; fliplr(refSe2')'];
-
 % Create figure
-figure()
-plot(refTD.mean_temp, refTD.layerCenter, 'LineWidth', lineWidth, 'Color', refColorDark, 'DisplayName', ...
-    'Reference Mean');
-hold on
-fill(refX2, refY2, refColorLight(1:3), 'FaceAlpha', refColorLight(4), 'EdgeColor', 'none', 'DisplayName', 'Reference Mean±SE');
-hold on
-plot(mhwTD.mean_temp, mhwTD.layerCenter, 'LineWidth', lineWidth, 'Color', mhwColorDark, 'DisplayName', ...
-    'MHW Mean');
-hold on
-fill(mhwX2, mhwY2, mhwColorLight(1:3), 'FaceAlpha', mhwColorLight(4), 'EdgeColor', 'none', 'DisplayName', 'MHW Mean±SE');
-legend('Location', 'northwest')
-fontsize(16, 'points')
-ylim([0, depthLim])
-xlabel("Temperature [˚C]"), ylabel("Depth (m)"), title(['Tasman Sea Upper ' char(string(depthLim)) 'm T-D'])
-grid on
-set(gca, 'YDir','reverse')
-hold off
+t = tiledlayout("horizontal");
+
+for i = 1:numMonths %[1 2 12]
+    nexttile
+
+    % subset for the month
+    refTDsub = refTD(refTD.floatMonth == months(i),:);
+    mhwTDsub = mhwTD(mhwTD.floatMonth == months(i),:);
+
+    % calculate standard error shapes
+    mhwSe1 = mhwTDsub.mean_temp + mhwTDsub.se;
+    mhwSe2 = mhwTDsub.mean_temp - mhwTDsub.se;
+    mhwY2 = [mhwTDsub.layerCenter; fliplr(mhwTDsub.layerCenter')'];
+    mhwX2 = [mhwSe1; fliplr(mhwSe2')'];
+    
+    refSe1 = refTDsub.mean_temp + refTDsub.se;
+    refSe2 = refTDsub.mean_temp - refTDsub.se;
+    refY2 = [refTDsub.layerCenter; fliplr(refTDsub.layerCenter')'];
+    refX2 = [refSe1; fliplr(refSe2')'];
+
+    % draw plot
+    line1 = plot(refTDsub.mean_temp, refTDsub.layerCenter, 'LineWidth', lineWidth, 'Color', refColorDark, 'DisplayName', ...
+        'Reference Mean');
+    hold on
+    line2 = fill(refX2, refY2, refColorLight(1:3), 'FaceAlpha', refColorLight(4), 'EdgeColor', 'none', 'DisplayName', 'Reference Mean±SE');
+    hold on
+    line3 = plot(mhwTDsub.mean_temp, mhwTDsub.layerCenter, 'LineWidth', lineWidth, 'Color', mhwColorDark, 'DisplayName', ...
+        'MHW Mean');
+    hold on
+    line4 = fill(mhwX2, mhwY2, mhwColorLight(1:3), 'FaceAlpha', mhwColorLight(4), 'EdgeColor', 'none', 'DisplayName', 'MHW Mean±SE');
+
+    fontsize(16, 'points')
+    ylim([0, depthLim]), xlim(xLim)
+    set(gca, 'YDir','reverse')
+    grid on
+    title(datestr(datetime(1, months(i),1),'mmmm'))
+    hold off
+end
+
+% overall properties
+lgd = legend();
+lgd.Layout.Tile = 'north';
+xlabel(t, "Temperature [˚C]"), ylabel(t, "Depth (m)")
+title(t, ['Tasman Sea Upper ' char(string(depthLim)) 'm T-D'])
+
 
 % Save figure
-print(['Tasman_' refYear '_' mhwYear '_TD_' char(string(depthLim)) 'm'], '-dpng')
+print(['Tasman_' refYear '_' mhwYear '_monthly_TD_' char(string(depthLim)) 'm'], '-dpng')
